d548693033ce9cd3d59bce223a2e379a4b896cd5
Ankith Thalanki <ankiththalanki2005@gmail.com>
2026-03-07 22:11:40 -0500

    Fixed issue with auth and aded polling to docker compose

--- src/utils/authUser.ts
+++ src/utils/authUser.ts
@@ -9,11 +9,12 @@ export interface AuthUser {
 
 export async function getUserFromRequest(): Promise<AuthUser> {
   const session = await auth();
-  if (!session || !session.user || !session.user.id || !session.user.type) {
+  if (!session || !session.user || !session.user.userId || !session.user.type) {
     throw new Error("Invalid session or missing user info");
+    
   }
   return {
-    userId: session.user.id,
+    userId: session.user.userId,
     type: session.user.type as UserType,
   };
 }
