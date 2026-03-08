2dd4550a5916189e0f012fd1210c0900e5f2b57d
Ankith Thalanki <ankiththalanki2005@gmail.com>
2026-03-07 23:08:16 -0500

    Reduced number of session refreshes

--- src/utils/authUser.ts
+++ src/utils/authUser.ts
@@ -11,7 +11,6 @@ export async function getUserFromRequest(): Promise<AuthUser> {
   const session = await auth();
   if (!session || !session.user || !session.user.userId || !session.user.type) {
     throw new Error("Invalid session or missing user info");
-    
   }
   return {
     userId: session.user.userId,
